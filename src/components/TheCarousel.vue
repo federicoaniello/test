d4535d2b04ec173cf3434b42f98c747e1c4fa761
Federico Aniello <federico.aniello@exprivia.com>
2023-01-27 23:21:55 +0100

    Minor fixes

--- src/components/TheCarousel.vue
+++ src/components/TheCarousel.vue
@@ -1,76 +1,92 @@
 <template>
-    <swiper :modules="[Navigation]" :slides-per-view="1" :allow-touch-move="true"
-        :prevent-interaction-on-transition="true" :navigation="false" :space-between="0" :centered-slides="false"
-        :loop="true" :breakpoints="{
+    <swiper
+        :modules="[Navigation]"
+        :slides-per-view="1"
+        :allow-touch-move="true"
+        :prevent-interaction-on-transition="false"
+        :navigation="false"
+        :space-between="0"
+        :centered-slides="true"
+        :loop="true"
+        :breakpoints="{
             768: {
+                preventInteractionOnTransition: true,
                 allowTouchMove: false,
                 slidesPerView: 1.2,
-                centeredSlides: true,
                 navigation: {
                     enabled: true,
                     nextEl: '.image-swiper-button-next',
                     prevEl: '.image-swiper-button-prev',
-                }
+                },
             },
-        }" @swiper="onSwiper" @slideChange="onSlideChange">
+        }"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+    >
         <swiper-slide v-for="(slide, index) in swiper_data" :key="index">
             <main class="overlay-buttons">
                 <button class="image-swiper-button-prev"></button>
                 <button class="image-swiper-button-next"></button>
             </main>
-            <section>
-                <div>
-                    <img :src="slide.img" alt="">
-                    <div v-if="slide.titles" class="carousel--wrapper">
-                        <div v-html="slide.titles.first"></div>
-                        <div class="second-div">{{ slide.titles.second }}<span><img class="ml-3"
-                                    :src="'/svg/right-arrow.svg'" alt=""></span></div>
+            <div class="img-wrapper">
+                <img :src="slide.img" alt="" />
+                <div v-if="slide.titles" class="carousel--wrapper">
+                    <div v-html="slide.titles.first"></div>
+                    <div class="second-div">
+                        {{ slide.titles.second
+                        }}<span
+                            ><img
+                                class="ml-3"
+                                :src="'/svg/right-arrow.svg'"
+                                alt=""
+                        /></span>
                     </div>
                 </div>
-            </section>
+            </div>
         </swiper-slide>
     </swiper>
 </template>
 
 <script setup>
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation } from "swiper";
-import { swiper_data } from './data';
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation } from 'swiper'
+import { swiper_data } from './data'
 // Import Swiper styles
-import 'swiper/scss';
+import 'swiper/scss'
 
-const onSwiper = (swiper) => {
-};
+const onSwiper = (swiper) => {}
 const onSlideChange = () => {
-    console.log('slide change');
-};
+    console.log('slide change')
+}
 </script>
 
 <style lang="scss" scoped>
 .swiper-slide {
-    @media (max-width:767px) {
+    @media (max-width: 767px) {
         width: 100vw;
 
-        >img {
+        > img {
             overflow: hidden;
         }
     }
 
     &-active {
-         @media (max-width:767px) {
-            z-index: 1; 
-        }
         .overlay-buttons {
             display: flex;
-            
-            @media (max-width:767px) {
+
+            @media (max-width: 767px) {
                 display: none;
             }
         }
     }
 }
 
-
+.img-wrapper {
+    @media (max-width: 767px) {
+        overflow: hidden;
+        width: 100vw;
+    }
+}
 .overlay-buttons {
     display: none;
     opacity: 0;
@@ -84,12 +100,11 @@ const onSlideChange = () => {
     top: 0;
     bottom: 0;
 
-
     &:hover {
         opacity: 1;
     }
 
-    >button {
+    > button {
         width: 70px;
         height: 100px;
         border: none;
@@ -103,7 +118,6 @@ const onSlideChange = () => {
                 justify-content: center;
                 align-items: center;
                 content: url('/svg/right-arrow-transparent.svg');
-
             }
         }
 
@@ -113,50 +127,39 @@ const onSlideChange = () => {
     }
 }
 
-
-
 img {
-    @media (min-width:768px) {
+    @media (min-width: 768px) {
         width: 100%;
     }
 }
 
-section {
-    position: relative;
+.carousel--wrapper {
+    position: absolute;
+    color: white;
+    font-size: 54px;
+    text-align: center;
+    font-weight: lighter;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-    >div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
+    @media (max-width: 767px) {
+        font-size: 34px;
+    }
+    .second-div {
+        font-size: 18px;
+        font-weight: lighter;
 
-        .second-div {
-            font-size: 18px;
-            font-weight: lighter;
-
-            img {
-                width: 12px;
-            }
-
-            span {
-
-                margin-left: 10px;
-            }
+        img {
+            width: 12px;
         }
 
-
-        .carousel--wrapper {
-            position: absolute;
-            color: white;
-            font-size: 54px;
-            text-align: center;
-            font-weight: lighter;
-
-            @media (max-width:767px) {
-                font-size: 34px;
-            }
+        span {
+            margin-left: 10px;
         }
-
     }
 }
 </style>
